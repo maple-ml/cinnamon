b14e700ea24fdf4f92e5acd307e3195e52560263
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2022-06-09 22:01:44 -0700

    add a simple example

---
+++ examples/sprite_test.py
@@ -0,0 +1,24 @@
+# this file goes in Geometry Dash/cinnamon/mods/ (eventually will have to be in a mod folder)
+
+import cinnamon
+import geometrydash as gd
+from cocos2d import *
+
+class MenuLayer:
+    def init(self):
+        gd.MenuLayer.initO(self)
+        sprite = CCSprite("GJ_button_01.png")
+        
+        self.addChild(sprite)
+
+class SpriteTest(cinnamon.Mod):
+    def __init__(self):
+        cinnamon.Mod.__init__(self)
+        self.name = "sprite_test"
+        self.version = "0.0.1"
+        self.author = "spook"
+        self.description = "Adds a sprite to the screen!"
+        
+        cinnamon.hook(gd.MenuLayer.initN, gd.MenuLayer.initA, MenuLayer.init)
+
+cinnamon.register_mod(SpriteTest)
